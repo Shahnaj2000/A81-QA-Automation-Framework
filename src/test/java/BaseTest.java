22b0f77e4e50219a6f9e2b6ca2661f6f6add441a
shahnajkhatun117 <shahnaj.khatun@testpro.io>
2026-04-15 15:57:46 -0500

    creating the test case for playing song

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,55 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
 
 public class BaseTest {
+    public WebDriver driver;
+    String url = "https://qa.koel.app/";
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+    }
+    @BeforeMethod
+    public void launchBrowser() {
+        //Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        //Pre-condition
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+    @AfterMethod
+    public void closeBrowser(){
+        driver.quit();
+    }
+    public void navigateToPage () {
+        driver.get(url);
+    }
+    public  void provideEmail(String email ) {
+        WebElement emailField = driver.findElement(By.xpath("//input[@type='email']"));
+        emailField.click();
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+    public void providePassword(String password) {
+        WebElement passwordField = driver.findElement(By.xpath("//input[@type='password']"));
+        passwordField.click();
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+    public void clickLoginButton() {
+        WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginButton.click();
     }
 }
--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,51 +1,21 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 public class Homework18 extends BaseTest  {
-    WebDriver driver;
     @Test
-    public void playSong() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    public void playSong() throws InterruptedException {
         navigateToPage();
         provideEmail("shahnaj.khatun@testpro.io");
         providePassword("Faizan@123");
         clickLoginButton();
         clickPlay();
+        Thread.sleep(2000);
         boolean isSoundBarDisplayed = isSongPlaying();
         Assert.assertTrue(isSoundBarDisplayed);
+    }
 
-    }
-    public void navigateToPage () {
-        String url = "https://qa.koel.app/";
-        driver.get(url);
-    }
-    public  void provideEmail(String email ) {
-        WebElement emailField = driver.findElement(By.xpath("//input[@type='email']"));
-        emailField.click();
-        emailField.clear();
-        emailField.sendKeys(email);
-    }
-       public void providePassword(String password) {
-        WebElement passwordField = driver.findElement(By.xpath("//input[@type='password']"));
-        passwordField.click();
-        passwordField.clear();
-        passwordField.sendKeys(password);
-    }
-    public void clickLoginButton() {
-        WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
-        loginButton.click();
-    }
     public void clickPlay() {
         WebElement playNextSong = driver.findElement(By.xpath(" //i[@data-testid='play-next-btn']"));
         WebElement playButton = driver.findElement(By.xpath("//span[@class='play']"));
