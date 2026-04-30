de9a699ef53d148967e2a6a489dc2e009004db67
shahnajkhatun117 <shahnaj.khatun@testpro.io>
2026-04-29 21:19:40 -0500

    Implementing Page Factory in previous homework

--- src/test/java/Homework23.java
+++ src/test/java/Homework23.java
@@ -3,26 +3,19 @@ import POM.LoginPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Homework21 extends BaseTest{
+public class Homework23 extends BaseTest{
     String newRenamePlaylist = "Sample Edited Playlist";
     @Test
-    public void renamePlaylist(){
-        String updatedMsg ="Updated playlist \"Sample Edited Playlist.\"";
+    public void renamePlaylist() {
+        String updatedMsg = "Updated playlist \"Sample Edited Playlist.\"";
         LoginPage loginpage = new LoginPage(driver);
         HomePage homePage = new HomePage(driver);
-        loginpage .provideEmail("shahnaj.khatun@testpro.io");
-        loginpage .providePassword("Faizan@123");
+        loginpage.provideEmail("shahnaj.khatun@testpro.io");
+        loginpage.providePassword("Faizan@123");
         clickLoginButton();
         homePage.dbClickPlaylist();
         homePage.enterNewPlaylist();
-      Assert.assertEquals(homePage.getRenamePlaylistSuccessMsg(),updatedMsg);
-
+        Assert.assertEquals(homePage.getRenamePlaylistSuccessMsg(), updatedMsg);
     }
 
     }
-
-
-
-
-
-
--- src/test/java/POM/BasePage.java
+++ src/test/java/POM/BasePage.java
@@ -1,9 +1,10 @@
-package Pages;
+package POM;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -18,6 +19,7 @@ public BasePage(WebDriver givenDriver){
     driver =givenDriver;
     wait =new WebDriverWait( driver, Duration.ofSeconds(15));
     actions = new Actions(driver);
+    PageFactory.initElements(driver,this);
 }
 public WebElement findElement(By locator){
     return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
