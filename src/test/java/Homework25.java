a5d5f7c699717d0c491cea202b0f68401962d4fa
shahnajkhatun117 <shahnaj.khatun@testpro.io>
2026-05-02 22:03:08 -0500

    Creating  Lambdatest, cloud execution capability, parallel testing, including the usage of ThreadLocal for thread safety.

---
+++ src/test/java/Homework25.java
@@ -0,0 +1,4 @@
+public class Homework25 {
+  //  the homework-25 branch explaining that all changes are inside BaseTest. Inside the BaseTest (Implement parallel testing,
+    //  including the usage of ThreadLocal for thread safety, Lambdatest and  cloud execution)
+}
